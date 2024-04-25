c340a3d396a612e423cea9809b90e9d8b0d5addf
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-04-25 18:23:54 +0300

    Done with lab

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+// Write your algorithm here
+
+  const seenNumbers = {};
+
+  for (const number of array) {
+    const complement = target - number;
+
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
 }
 
-/* 
+
+/*
   Write the Big O time complexity of your function here
 */
+// Runtime: O(n^2)
+// function hasTargetSum(array, target) {
+//  for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === complement) return true;
 
-/* 
+
+
+/*
   Add your pseudocode here
 */
+// create an empty set called seenNumbers
+
+// for each number in the array:
+//     calculate the complement = target - number
+
+//     if complement is in seenNumbers:
+//         return true
+//     else:
+//         add the number to seenNumbers
+
+// return false
+
 
 /*
   Add written explanation of your solution here
 */
+// Go through each number in the array.
+// For each number, check if its complement is in the set.
+// Add each number to the set as we check.
+// If we find a complement, return true.
+// If we check all numbers without finding a match, return false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
